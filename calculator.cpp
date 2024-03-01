8fa90c843ebf1d332713674db9f177c54735d2c0
kiejibi <kaekie2004@gmail.com>
2024-03-01 13:43:09 +0700

    completed

--- calculator.cpp
+++ calculator.cpp
@@ -131,7 +131,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wc.hCursor	 = LoadCursor(NULL, IDC_ARROW);
 
 	/* White, COLOR_WINDOW is just a #define for a system color, try Ctrl+Clicking it */
-	wc.hbrBackground = CreateSolidBrush(RGB(38,61,92));
+	wc.hbrBackground = CreateSolidBrush(RGB(47,7,26));
 	wc.lpszClassName = "WindowClass";
 	wc.hIcon	 = LoadIcon(NULL, IDI_APPLICATION); /* Load a standard icon */
 	wc.hIconSm	 = LoadIcon(NULL, IDI_APPLICATION); /* use the name "A" to use the project icon */
